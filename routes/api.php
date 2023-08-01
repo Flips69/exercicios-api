dc2d43d52b4edcca8832a064194ac2a3185597cb
Flips69 <aion.lipe@gmail.com>
2023-08-01 15:15:51 -0300

    Aula For Request

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\ExercicioCincoController;
+use App\Http\Controllers\ExercicioDoisController;
+use App\Http\Controllers\ExercicioQuatroController;
+use App\Http\Controllers\ExercicioSeteController;
+use App\Http\Controllers\ExercicioTresController;
+use App\Http\Controllers\ExercicioUm;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +20,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('exercicio/um', 
+[ExercicioUm::class, 'multiplicar']);
+
+Route::post('exercicio/dois', 
+[ExercicioDoisController::class, 'retornarMaior']);
+
+Route::get('exercicio/sete', 
+[ExercicioSeteController::class, 'exibirNumeros']);
+
+Route::post('exercicio/quatro', 
+[ExercicioQuatroController::class, 'verificarNumero']);
+
+Route::post('exercicio/tres',
+[ExercicioTresController::class, 'media']);
+
+Route::post('exercicio/cinco',
+[ExercicioCincoController::class, 'verificar']);
